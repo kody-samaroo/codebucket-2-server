88f6c289923df337a42a5857d48914c3bcab366f
Kody Samaroo <kody.samaroo@gmail.com>
2023-11-28 21:10:37 -0500

    first commit; setting up server

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "blue",
+    database: "codebucket",
+    port: 9000
+});
+
+// db.connect((error) => {
+//     if (error) {
+//       console.error('Error connecting to MySQL database:', error);
+//     } else {
+//       console.log('Connected to MySQL database!');
+//     }
+//   });
+
+app.get("/", (req,res) => {
+    res.json("hello");
+})
+
+// GET ALL PROJECTS
+app.get("/projects", (req,res)=> {
+    const q = "SELECT * FROM projects"
+    db.query(q, (err,data)=> {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+// CREATE PROJECT
+app.post("/projects", (req,res) => {
+    const q = "INSERT INTO projects (`project_id`, `title`, `author`, `html`, `css`, `js`) VALUES (?)"
+    const values = [
+        req.body.project_id,
+        req.body.title,
+        req.body.author,
+        req.body.html,
+        req.body.css,
+        req.body.js
+    ];
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Project has been added");
+    })
+})
+
+app.listen(8800, () => {
+    console.log("Connected to backend");
+});
